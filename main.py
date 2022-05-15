eb46cb74f0d8dbf5afd5f548473ab73a746ad6e6
Ioannis Georgiades <I.GEORGIADES@student.tudelft.nl>
2022-05-15 22:13:23 +0200

    fix the __main__

--- main.py
+++ main.py
@@ -2,17 +2,6 @@ import cv2
 from PIL import Image
 from pytesseract import pytesseract
 
-camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-
-while True:
-    ret, image = camera.read()
-    cv2.imshow('Text detection', image)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        cv2.imwrite('image.jpg', image)
-        break
-camera.release()
-cv2.destroyAllWindows()
-
 
 def tesseract():
     path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -22,4 +11,15 @@ def tesseract():
     print(text[:-1])
 
 
-tesseract()
+if __name__ == '__main__':
+    camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+    while True:
+        ret, image = camera.read()
+        cv2.imshow('Text detection', image)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.imwrite('image.jpg', image)
+            break
+    camera.release()
+    cv2.destroyAllWindows()
+    tesseract()
