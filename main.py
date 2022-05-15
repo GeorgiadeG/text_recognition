af910a827469b661a828352bda4475134f38f7e9
Ioannis Georgiades <I.GEORGIADES@student.tudelft.nl>
2022-05-15 22:01:30 +0200

    basic program for webcam text recogntion

---
+++ main.py
@@ -0,0 +1,25 @@
+import cv2
+from PIL import Image
+from pytesseract import pytesseract
+
+camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+while True:
+    ret, image = camera.read()
+    cv2.imshow('Text detection', image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imwrite('image.jpg', image)
+        break
+camera.release()
+cv2.destroyAllWindows()
+
+
+def tesseract():
+    path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    image_path = r'image.jpg'
+    pytesseract.tesseract_cmd = path_to_tesseract
+    text = pytesseract.image_to_string(Image.open(image_path))
+    print(text[:-1])
+
+
+tesseract()
